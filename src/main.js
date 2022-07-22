689cc581c271e3a4fc80a12f5a3d66255e13351e
limu <1259567994@qq.com>
2022-07-22 21:29:30 +0800

    2

--- src/main.js
+++ src/main.js
@@ -1,8 +1,55 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+import VueRouter from "vue-router";
+import main from "./views/main.vue";
+import news from "./views/news.vue";
+import sports from "./views/sports.vue";
+import sportsMain from "./components/sports-main.vue";
+import sportsIn from "./components/sports-in.vue";
+import sportsOut from "./components/sports-out.vue";
+Vue.use(VueRouter);
+const routes = [
+  {
+    path: "/", // 默认hash值路径
+    redirect: "/main"
+  },
+  {
+    path: "/main",
+    component: main,
+  },
+  {
+    path: "/news",
+    component: news,
+  },
+  {
+    path: "/sports",
+    component: sports,
+    redirect: "/sports/sportsMain",
+    children: [
+      {
+        path: "sportsMain",
+      component: sportsMain,
+      },
+      {
+        path: "sportsIn",
+      component: sportsIn,
+      },
+      {
+        path: "sportsOut",
+      component: sportsOut,
+      }
+    ],
+  },
+ 
+];
+const router = new VueRouter({
+  routes,
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ src/views/sports.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p><b>体育·改变人生</b></p>
+
+    <div>
+      <router-link to="/sports/sportsMain">主页</router-link>
+      <router-link to="/sports/sportsIn">新闻</router-link>
+      <router-link to="/sports/sportsOut">体育</router-link>
+    </div>
+    <router-view></router-view>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "T-Sports",
+};
+</script>
+
+<style></style>
